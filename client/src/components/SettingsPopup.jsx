b74c87fb6957f1855255126e429e200c9c815a47
AdmireLinK <64939678+AdmireLinK@users.noreply.github.com>
2025-11-29 17:29:46 +0800

    chore：调整血战模式介绍

--- client/src/components/SettingsPopup.jsx
+++ client/src/components/SettingsPopup.jsx
@@ -463,7 +463,7 @@ function SettingsPopup({ gameSettings, onSettingsChange, onClose, onRestart, hid
                     启用后，第一个猜对的玩家不会立即结束游戏。<br/>
                     所有玩家继续猜测，直到最后一人猜对或次数耗尽。<br/>
                     得分：第1名得(玩家数)分，第2名得(玩家数-1)分...依此类推。<br/>
-                    出题人：有人猜中得1分，无人猜中扣2分。
+                    出题人：猜对的人数过多和过少都会降低得分，无人猜对扣分。
                   </span>
                 </span>
                 <input
