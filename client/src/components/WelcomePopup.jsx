555ad11705cf7401b0fbe86592018995e9c8903a
AdmireLinK <64939678+AdmireLinK@users.noreply.github.com>
2025-12-16 17:24:24 +0800

    Revert "chore：临时调整首页公告"
    This reverts commit f63e532651eaf8d3fa563a0a44f79d616ba0fb6f.

--- client/src/components/WelcomePopup.jsx
+++ client/src/components/WelcomePopup.jsx
@@ -27,18 +27,21 @@ function WelcomePopup({ onClose }) {
         <div className="popup-body">
           <div className="welcome-content">
             <div className="welcome-text">
-              <p className="vote-main-text">
-                投票给<span className="highlight-kanade">立华奏</span>喵<br/>
-                投票给<span className="highlight-kanade">立华奏</span>谢谢喵
-              </p>
-              <p className="vote-sub-text">
-                现在立刻加入猜猜呗官方Q群参与第二届CCB Moe投票 为<span className="highlight-kanade">立华奏</span>献上宝贵的一票
-              </p>
-              <div className="vote-action">
-                <a href="https://qm.qq.com/q/2sWbSsCwBu" target="_blank" rel="noopener noreferrer" className="join-qq-btn">
-                  <i className="fab fa-qq"></i> 加入QQ群
-                </a>
-              </div>
+              <p>猜猜呗终于<del>和Faze一起</del>地狱归来了！这段时间我们对多人模式进行了大量更新：</p>
+              <ul>
+                <li><b>血战模式</b>：第一个猜对的玩家不会立即结束游戏，所有玩家继续猜测，直到最后一人猜对或次数耗尽</li>
+                <li><b>同步模式</b>：所有玩家需要等待其他玩家完成当前轮猜测后才能进行下一轮</li>
+                <li><b>角色全局BP</b>：开启后一名角色只能被猜一次（除答案）</li>
+                <li><b>标签全局BP</b>：开启后一个标签只能被首个猜到的玩家获取</li>
+                <li>此外，还有<b>大量UIUX优化和Bug修复</b></li>
+              </ul>
+              感谢 <a href="https://github.com/trim21" target="_blank" rel="noopener noreferrer">Bangumi 管理员</a> 的优化支持，
+                以及各位<a href="https://github.com/kennylimz/anime-character-guessr/graphs/contributors" target="_blank" rel="noopener noreferrer">网友</a>贡献的代码和数据。
+                感谢大家这段时间的热情和支持
+              <br/>
+              <p>另外，我们的QQ群正在火热招新中，群中即将举办新一届新人杯，此后还将有新一届CCB Major等赛事，参与无门槛，获胜有奖品，欢迎点击主页下方按钮或扫描Logo旁的二维码加入！</p>
+
+              <p><b>如果您有任何建议或问题，欢迎加入我们的<a href="https://qm.qq.com/q/2sWbSsCwBu" target="_blank" >QQ群</a>或<a href="https://github.com/kennylimz/anime-character-guessr/issues/new" target="_blank" >提交Issue</a>！</b></p>
               
               <hr style={{margin: '20px 0', border: '0', borderTop: '1px solid rgba(0,0,0,0.1)'}} />
               
